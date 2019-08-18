d6eaf3f7da6368a802f53a7ea984ef7eb83649f6
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-18 22:52:52 +0530

    Update prob04.py

--- prob04.py
+++ prob04.py
@@ -1,41 +1,23 @@
-
 def palindrome(num):
     tot=0
-    x=num
-    
-    
+    x=num   
     while num!=0:        
         d=num%10
         tot=tot*10+d
-        num//=10        
-    
+        num//=10         
     else:
         return tot==x 
 
 pali=[]
 def lpali():
-    a=99
-    b=100
-    while a and b :
-        a+=1
-        b=100         
-        while 100<b<999 and 100<a<999:
-            if palindrome(a*b)==True:
-                pali.append(a*b)
-                b+=1 
-            else:
-                b+=1
-           
+    ans=list(a*b for a in range(100,1000) for b in range(100,1000))
+    for item in ans:
+        if palindrome(item)==True:
+            pali.append(item)
+    return max(pali)
+print( lpali())
 
-        #if palindrome(a*b)==True:
-        #    pali.append(a*b)
-        #    a+=1
-        #    b+=1
-        #else:
-        #    a+=1
-        #    b+=1
-    return(pali)
-print(lpali())
+
 
 
    
