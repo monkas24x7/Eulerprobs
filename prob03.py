9be7568fc60d522703c6dcdc8947cdaa958a1d16
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-19 03:20:15 +0530

    Update prob03.py

--- prob03.py
+++ prob03.py
@@ -1,24 +1,16 @@
-def maxprimefac(n):
-    p=2 
-    
-    primenos=[] 
-    primefacs=[]
-    x = [True for y in range(0,n)]    
-    while (p * p <= n):              
-        if x[p] == True:            
-            for y in range(p * p, n, p): 
-                x[y] = False        
-        p += 1       
-    for p in range(2, n): 
-        if x[p]==True:
-            primenos.append(p) 
-    for item in primenos:
-        if n%item==0:
-            primefacs.append(p)          
-            
-        
-    return max(primefacs)
-print(maxprimefac(600851475143))
+def maxprime(num):
+    x = 2
+    p_factors = []
+    while x*x<= num:
+        if num %x:
+            x=x+ 1
+        else:
+            num=num/x
+            p_factors.append(x)
+    if num > 1:
+        p_factors.append(num)
+    return max(p_factors)
+print(maxprime(600851475143))  
 
     
 
