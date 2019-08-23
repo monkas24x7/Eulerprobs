cd5338e27b6af6de446617c6d882c4b48a5bb17e
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-24 02:14:18 +0530

    Update prob07.py

--- prob07.py
+++ prob07.py
@@ -1,10 +1,10 @@
 def primefind(n):     
-    c=0      
+    c=1      
     x = [True for y in range(0,n+1)]    
     for p in range(4,n+1,2):
         x[p]= False
     for p in range(3, n+1, 2):
-        if c<=9999:
+        if c<10001:
             if x[p] == True:
                 z=p
                 c+=1            
