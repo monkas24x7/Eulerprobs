a77572692ae27c3c538c4158ced93f1790d245cb
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-23 22:25:03 +0530

    Update prob10.py

--- prob10.py
+++ prob10.py
@@ -1,14 +1,12 @@
 def primesum(n):    
-    primes=[]        
+    t=2        
     x = [True for y in range(0,n+1)]    
     for p in range(4,n+1,2):
         x[p]= False
     for p in range(3, n+1, 2):
-        if x[p] == True:            
+        if x[p] == True:
+            t+=p            
             for y in range(p * p, n+1, 2*p): 
                 x[y] = False         
-    for p in range(2, n): 
-        if x[p]==True and n%x[p]==0: 
-            primes.append(p)
-    return sum(primes)
+    return t
 print(primesum(2000000))
