b0907ee933d6131d19b8353b00b16c7f7cdef498
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-23 22:14:46 +0530

    Update prob07.py

--- prob07.py
+++ prob07.py
@@ -1,15 +1,16 @@
 def primefind(n):     
-    primes=[]      
+    c=0      
     x = [True for y in range(0,n+1)]    
     for p in range(4,n+1,2):
         x[p]= False
     for p in range(3, n+1, 2):
-        if x[p] == True:            
-            for y in range(p * p, n+1, 2*p): 
-                x[y] = False        
-          
-    for p in range(2, n): 
-        if x[p]==True: 
-            primes.append(p)
-    return primes[10000]
+        if c<=9999:
+            if x[p] == True:
+                z=p
+                c+=1            
+                for y in range(p * p, n+1, 2*p): 
+                    x[y] = False
+        else:
+            break          
+    return z
 print(primefind(3000000))
