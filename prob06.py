2c9d987a2c06922602d155133e923f6719052d7f
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-23 17:32:26 +0530

    Update prob06.py

--- prob06.py
+++ prob06.py
@@ -1,13 +1,12 @@
 def sqdiff():
+    n=100
     def sqtot():
-        tot=0
-        for num in range(1,101):
-            tot=tot+num**2
+        tot= n*(n+1)*(2*n+1)/6
         return tot   
     def totsq():
-        for num in range(1,101):
-            tot2=sum(range(1,101))**2
+        tot2=(sum(range(1,101)))**2
         return tot2
     return totsq()-sqtot()   
 print(sqdiff())
+
 
