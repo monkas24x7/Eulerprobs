fd7cea8e0470089e5402ef3a93e703f8a2eff3d9
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-25 01:41:11 +0530

    Update prob10.py

--- prob10.py
+++ prob10.py
@@ -1,12 +1,16 @@
-def primesum(n):    
-    t=2        
-    x = [True for y in range(0,n+1)]    
-    for p in range(4,n+1,2):
-        x[p]= False
-    for p in range(3, n+1, 2):
-        if x[p] == True:
-            t+=p            
-            for y in range(p * p, n+1, 2*p): 
-                x[y] = False         
-    return t
-print(primesum(2000000))
+def primesum():   
+    z=3
+    tot=2   
+    def primecheck(n):        
+        p = 2       
+        while p*p <= n:
+            if n % p == 0:                
+                return False
+            p += 1        
+        return True
+    while z<=2000000:
+        if primecheck(z)==True:
+            tot+=z            
+        z+=2
+    return tot
+print(primesum())
