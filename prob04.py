3ad0ded8c7df7264ff7451ea39d5bfa5959689ba
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-24 21:31:21 +0530

    Update prob04.py

--- prob04.py
+++ prob04.py
@@ -1,20 +1,9 @@
-def palindrome(num):
-    tot=0
-    x=num   
-    while num!=0:        
-        d=num%10
-        tot=tot*10+d
-        num//=10         
-    
-    return tot==x 
-
-pali=[]
 def lpali():
     z=0
     for a in range(100,1000):
         for b in range(100,1000):
             a*b
-            if palindrome(a*b)==True and a*b>z:
+            if str(a*b)==str(a*b)[::-1] and a*b>z:
                 z=a*b
     return z
 
