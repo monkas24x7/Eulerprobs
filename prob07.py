4fcff63ead8ae9a69dda54fdac87dc0e4486df28
monkas24x7 <51705194+monkas24x7@users.noreply.github.com>
2019-08-25 11:56:43 +0530

    Update prob07.py

--- prob07.py
+++ prob07.py
@@ -1,18 +1,21 @@
-def Optimus_prime():    
+def prime():    
     c=1
+    z=3
     def primecheck(n):        
-        p = 2       
+        p = 3
+        if n%2==0:
+            return False       
         while p*p <= n:
             if n % p == 0:                
                 return False
-            p += 1        
+            p+=2       
         return True
-    z=3
     while c<=10000:
         if primecheck(z):
             c+=1
             promo=z
         z+=2
     return promo
-print(Optimus_prime())
+print(prime())
+
 
